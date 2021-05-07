961ea5d50f23b3db717a0a06aa2ddac5d9f6f09b
BERSY S B <bersysilvesterbatrish@gmail.com>
2021-05-07 16:41:57 +0530

    Add files via upload

---
+++ Day16_Activity/DriverManagement/DriverBL.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using DriverDALLibrary;
+using System.Linq;
+
+namespace DriverBLLibrary
+{
+    public class DriverBL : IRepo<Driver>
+    {
+            DriverDAL dal;
+            public DriverBL()
+            {
+                dal = new DriverDAL();
+            }
+
+            public bool Add(Driver t)
+            {
+                try
+                {
+                    return dal.AddDriver(t);
+                }
+                catch (Exception exception)
+                {
+                    throw exception;
+                }
+            }
+
+            public ICollection<Driver> GetAll()
+            {
+                List<Driver> drivers;
+                try
+                {
+                    drivers = dal.SelectAllDrivers().ToList();
+                    return drivers;
+                }
+                catch (Exception exception)
+                {
+
+                    throw exception;
+                }
+            }
+
+            
+        public bool UpdatePhone(Driver t)
+        {
+            try
+            {
+                return dal.UpdatePhone(t);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public bool UpdateStatus(Driver t)
+        {
+            try
+            {
+                return dal.UpdateDriverStatus(t);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        
+        
+    }
+}
---
+++ Day16_Activity/DriverManagement/IRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DriverBLLibrary
+{
+    public interface IRepo<T>
+    {
+        bool Add(T t);
+        bool UpdatePhone(T t);
+        bool UpdateStatus(T t);
+        ICollection<T> GetAll();
+    }
+}
