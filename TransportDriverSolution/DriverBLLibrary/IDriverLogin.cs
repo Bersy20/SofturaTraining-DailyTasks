23842ac689efcdefaf644881d68bd338b1415739
BERSY S B <bersysilvesterbatrish@gmail.com>
2021-05-07 15:40:20 +0530

    Add files via upload

---
+++ TransportDriverSolution/DriverBLLibrary/IDriverLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DriverBLLibrary
+{
+    public interface IDriverLogin<T>
+    {
+        bool Add(T t);
+        bool Login(T t);
+    }
+}
