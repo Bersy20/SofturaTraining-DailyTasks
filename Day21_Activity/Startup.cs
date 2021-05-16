11520e18e82b124cd56a59d8f1508ab343d980ee
BERSY S B <bersysilvesterbatrish@gmail.com>
2021-05-16 20:09:07 +0530

    Update Startup.cs

--- Day21_Activity/Startup.cs
+++ Day21_Activity/Startup.cs
@@ -25,7 +25,7 @@ namespace BooksWebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<PublicationContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:pubCon"]));
+            services.AddDbContext<BookContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:pubCon"]));
             services.AddControllersWithViews();
             services.AddScoped<IRepo<Book>, BookManager>();
 
