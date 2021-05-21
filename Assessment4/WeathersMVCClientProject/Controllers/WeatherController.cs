bacc3b75651e6a62d1ba651c14aa529a8ad49a54
BERSY S B <bersysilvesterbatrish@gmail.com>
2021-05-21 21:58:50 +0530

    Add files via upload

---
+++ Assessment4/WeathersMVCClientProject/Controllers/WeatherController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using WeathersMVCClientProject.Models;
+
+namespace WeathersMVCClientProject.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            string Baseurl = "http://localhost:63839/";
+            var WeatherInfo = new List<Weather>();
+            //HttpClient cl = new HttpClient();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Weathers");
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var WeatherResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    WeatherInfo = JsonConvert.DeserializeObject<List<Weather>>(WeatherResponse);
+
+                }
+                //returning the employee list to view  
+                return View(WeatherInfo);
+            }
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Weather p)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(p), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("http://localhost:63839/api/Weathers", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var obj = JsonConvert.DeserializeObject<Weather>(apiResponse);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<ActionResult> Details(string city)
+        {
+            Weather b = new Weather();
+            using (var httpClient = new HttpClient())
+            {
+
+                using (var response = await httpClient.GetAsync("http://localhost:63839/api/Weathers/" + city))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    b = JsonConvert.DeserializeObject<Weather>(apiResponse);
+                }
+            }
+            return View(b);
+        }
+        public async Task<ActionResult> Delete(string city)
+        {
+            TempData["City"] = city;
+            Weather b = new Weather();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:63839/api/Weathers/" + city))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    b = JsonConvert.DeserializeObject<Weather>(apiResponse);
+                }
+            }
+            return View(b);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Delete(Weather b)
+        {
+            string city = TempData["City"].ToString();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("http://localhost:63839/api/Weathers/" + city))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<ActionResult> Edit(string city)
+        {
+            TempData["City"] = city;
+            Weather b = new Weather();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:63839/api/Weathers/" + city))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    b = JsonConvert.DeserializeObject<Weather>(apiResponse);
+                }
+            }
+            return View(b);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Edit(Weather b)
+        {
+            int blogId = Convert.ToInt32(TempData["BlogId"]);
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(b), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PutAsync("http://localhost:7758/api/blogs/" + blogId, content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var obj = JsonConvert.DeserializeObject<Weather>(apiResponse);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Assessment4/WeathersMVCClientProject/Models/Weather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeathersMVCClientProject.Models
+{
+    public class Weather
+    {
+        public DateTime Date { get; set; }
+        [Key]
+        public string City { get; set; }
+        public float HighTemperature { get; set; }
+        public float LowTemperature { get; set; }
+        public string Forcast { get; set; }
+    }
+}
