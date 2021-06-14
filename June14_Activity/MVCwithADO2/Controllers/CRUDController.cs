7620aa5f749b4b31d2c28f60b46dd9c6e982a5d1
BERSY S B <bersysilvesterbatrish@gmail.com>
2021-06-14 15:37:18 +0530

    Add files via upload

---
+++ June14_Activity/MVCwithADO2/Controllers/CRUDController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCwithADO2.Models;
+
+namespace MVCwithADO2.Controllers
+{
+    public class CRUDController : Controller
+    {
+        public ActionResult Index()
+        {
+            CRUDModel mdl = new CRUDModel();
+            DataTable dt = mdl.DisplayBook();
+            return View("Home",dt);
+        }
+        public ActionResult Insert()
+        {
+            return View("Create");
+        }
+        public ActionResult InsertRecord(FormCollection frm,string action)
+        {
+            if(action=="Submit")
+            {
+                CRUDModel mdl = new CRUDModel();
+                string Title = frm["txtTitle"];
+                int aid = Convert.ToInt32(frm["txtAid"]);
+                double price = Convert.ToDouble(frm["txtPrice"]);
+                int rowIns = mdl.NewBook(Title, aid, price);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult IndexOfAuthors()
+        {
+            CRUDModel mdl = new CRUDModel();
+            DataTable dt = mdl.DisplayAuthors();
+            return View("IndexOfAuthors", dt);
+        }
+        public ActionResult InsertAuthorLink()
+        {
+            return View("CreateAuthor");
+        }
+        public ActionResult InsertAuthor(FormCollection form,string action)
+        {
+            if(action=="Submit")
+            {
+                CRUDModel model =new CRUDModel();
+                string AuthorName = form["txtAuthorName"];
+                int rowInsert = model.InsertNewAuthor(AuthorName);
+                return RedirectToAction("IndexOfAuthors");
+            }
+            else
+            {
+                return RedirectToAction("IndexOfAuthors");
+            }
+        }
+    }
+}
---
+++ June14_Activity/MVCwithADO2/Models/CRUDModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVCwithADO2.Models
+{
+    public class CRUDModel
+    {
+        public DataTable DisplayBook()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection("data source=.;database=BooksDB;Integrated Security=true");
+            SqlCommand cmd = new SqlCommand("select BookId,Title,AuthorID,Price from tbl_Books", con);
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        public int NewBook(string Title,int aid,double Price)
+        {
+            SqlConnection con = new SqlConnection("data source=.;database=BooksDB;Integrated Security=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("sp_InsertBook", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Title", Title);
+            cmd.Parameters.AddWithValue("@AuthorID", aid);
+            cmd.Parameters.AddWithValue("@Price", Price);
+            return cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public DataTable DisplayAuthors()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection("data source=.;database=BooksDB;Integrated Security=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select AuthorID,AuthorName from tbl_author",con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        public int InsertNewAuthor(string authorName)
+        {
+            SqlConnection con = new SqlConnection("data source=.;database=BooksDB;Integrated Security=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("sp_InsertAuthor", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@AuthorName", authorName);
+            return cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
